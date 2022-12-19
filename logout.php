09aefbf853f1eca936771f2f2f13332f532187f4
TBNR-1257 <tzehernbryan22w@forwardschool.edu.my>
2022-12-19 16:35:31 +0800

    first commit

---
+++ logout.php
@@ -0,0 +1,19 @@
+<?php 
+
+    session_start();
+
+    if ( isset( $_SESSION['user'] ) ){
+        // deletes user session data
+        unset ( $_SESSION['user'] );
+
+        //redirect user back to index
+        header ('Location: /');
+        exit;
+
+    } else {
+        // redirect to login page
+        header('Location: /login.php');
+        exit;
+    }
+    
+?>
